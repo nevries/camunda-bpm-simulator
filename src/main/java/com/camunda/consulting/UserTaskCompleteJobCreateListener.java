5e14a8d85c3b9be6108b2239e03e05396ad39cf3
Benjamin Hoffmann <benjamin.hoffmann@camunda.com>
2018-08-16 19:35:09 +0200

    refactor tasklistener

--- src/main/java/com/camunda/consulting/UserTaskCompleteJobCreateListener.java
+++ src/main/java/com/camunda/consulting/UserTaskCompleteJobCreateListener.java
@@ -1,42 +1,28 @@
 package com.camunda.consulting;
 
 import org.camunda.bpm.engine.delegate.DelegateTask;
-import org.camunda.bpm.engine.delegate.Expression;
 import org.camunda.bpm.engine.delegate.TaskListener;
-import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
-import org.camunda.bpm.engine.impl.context.Context;
+import org.camunda.bpm.engine.impl.el.Expression;
 import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
-import org.camunda.bpm.engine.impl.persistence.entity.ProcessDefinitionEntity;
-import org.camunda.bpm.engine.impl.persistence.entity.TimerEntity;
 import org.camunda.bpm.engine.impl.pvm.process.ActivityImpl;
-import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 
 import java.util.Date;
 import java.util.Optional;
 
-public class UserTaskCompleteJobCreateListener implements TaskListener{
+public class UserTaskCompleteJobCreateListener extends AbstractJobCreateListener implements TaskListener{
 
-    private Optional<Expression> nextFireExpression;
-    private ProcessEngineConfigurationImpl processEngineConfiguration;
+    ActivityImpl activity;
 
     public UserTaskCompleteJobCreateListener(ActivityImpl activity) {
-
-        this.processEngineConfiguration = SimulatorPlugin.getProcessEngineConfiguration();
-        BpmnModelInstance bpmnModelInstance ;
-        //= processEngineConfiguration.getDeploymentCache().findBpmnModelInstanceForProcessDefinition(activity.getProcessDefinition())
-
-        Optional<String> nextFire = Optional.empty();
-
-        //= ModelPropertyUtil.getNextFire(bpmnModelInstance.getModelElementById(activity.getActivityId()));
-
-        nextFireExpression = nextFire.map(processEngineConfiguration.getExpressionManager()::createExpression);
-
+      this.activity = activity;
     }
 
     @Override
     public void notify(DelegateTask task) {
 
-        if(nextFireExpression.isPresent()) {
+      Optional<Expression> nextFireExpression = getCachedNextFireExpression((ExecutionEntity) task.getExecution(), activity);
+
+      if(nextFireExpression.isPresent()) {
             Date dueDate = (Date) nextFireExpression.get().getValue(task.getExecution());
             createJobForUserTaskCompletion(task, dueDate);
         }
@@ -44,17 +30,11 @@ public class UserTaskCompleteJobCreateListener implements TaskListener{
 
     private void createJobForUserTaskCompletion(DelegateTask task, Date dueDate) {
 
-        TimerEntity timer = new TimerEntity();
-        ProcessDefinitionEntity processDefinition = ((ExecutionEntity) task.getExecution()).getProcessDefinition();
+      String jobHandlertype = CompleteUserTaskJobHandler.TYPE;
+      CompleteUserTaskJobHandler.CompleteUserTaskJobHandlerConfiguration jobHandlerConfiguration = new CompleteUserTaskJobHandler.CompleteUserTaskJobHandlerConfiguration(task.getId());
 
-        timer.setExecution((ExecutionEntity)task.getExecution());
-        timer.setDuedate(dueDate);
-        timer.setJobHandlerType(CompleteUserTaskJobHandler.TYPE);
-        timer.setProcessDefinitionKey(processDefinition.getKey());
-        timer.setDeploymentId(processDefinition.getDeploymentId());
-        timer.setJobHandlerConfiguration(new CompleteUserTaskJobHandler.CompleteUserTaskJobHandlerConfiguration(task.getId()));
+      createTimerJob((ExecutionEntity)task.getExecution(), jobHandlertype, dueDate, jobHandlerConfiguration);
 
-        //Context.getCommandContext().getJobManager().schedule(timer);
     }
 
 }
