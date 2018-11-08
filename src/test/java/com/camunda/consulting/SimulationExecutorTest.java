02e9722de1d3f2a14825609daaa5954ebac47125
Ragnar Nevries <ragnar.nevries@camunda.com>
2018-11-08 09:07:21 +0100

    Enhanced start event test case.

--- src/test/java/com/camunda/consulting/SimulationExecutorTest.java
+++ src/test/java/com/camunda/consulting/SimulationExecutorTest.java
@@ -1,12 +1,14 @@
 package com.camunda.consulting;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.condition.AnyOf.anyOf;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.init;
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.historyService;
 
 import java.util.List;
 
 import org.apache.ibatis.logging.LogFactory;
+import org.assertj.core.api.Condition;
 import org.camunda.bpm.engine.history.HistoricProcessInstance;
 import org.camunda.bpm.engine.history.HistoricVariableInstance;
 import org.camunda.bpm.engine.test.Deployment;
@@ -54,28 +56,54 @@ public class SimulationExecutorTest {
 
   @Test
   @Deployment(resources = "simulateStartTestModel.bpmn")
-  public void testSimulateStartEvent() {
+  public void testSimulateStartEventWithBusinessKeyAndPayload() {
     SimulationExecutor.execute(DateTime.now().plusHours(-1).toDate(), DateTime.now().toDate());
     long count = historyService().createHistoricProcessInstanceQuery().processDefinitionKey("oneStartPerMinute").completed().count();
     List<HistoricProcessInstance> list = historyService().createHistoricProcessInstanceQuery().processDefinitionKey("oneStartPerMinute").completed().list();
     for (HistoricProcessInstance historicProcessInstance : list) {
-      assertThat(historicProcessInstance.getBusinessKey()).isNotEmpty();
+      assertThat(historicProcessInstance.getBusinessKey()).is(anyOf(new StartsWithCondition("none"), new StartsWithCondition("message")));
     }
     long countFirst = 0;
     long countSecond = 0;
+    long countThird = 0;
     List<HistoricVariableInstance> list2 = historyService().createHistoricVariableInstanceQuery().processDefinitionKey("oneStartPerMinute").list();
     for (HistoricVariableInstance historicVariableInstance : list2) {
       if (historicVariableInstance.getName().equals("first")) {
         countFirst++;
-        assertThat(historicVariableInstance.getValue()).isEqualTo((int)1);
+        assertThat(historicVariableInstance.getValue()).isEqualTo((int) 1);
       }
       if (historicVariableInstance.getName().equals("second")) {
         countSecond++;
         assertThat(historicVariableInstance.getValue()).isEqualTo("2");
       }
+      if (historicVariableInstance.getName().equals("third")) {
+        countThird++;
+        assertThat(historicVariableInstance.getValue()).isEqualTo("message");
+      }
     }
+    // from none start event
     assertThat(countFirst).isEqualTo(59);
     assertThat(countSecond).isEqualTo(59);
-    assertThat(count).isEqualTo(59);
+
+    // from message start event
+    assertThat(countThird).isEqualTo(29);
+
+    // both
+    assertThat(count).isEqualTo(88);
+  }
+
+  public class StartsWithCondition extends Condition<String> {
+
+    String prefix;
+
+    public StartsWithCondition(String prefix) {
+      this.prefix = prefix;
+    }
+
+    @Override
+    public boolean matches(String value) {
+      return value.startsWith(prefix);
+    }
+
   }
 }
